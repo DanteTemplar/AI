2a87d8654d7964da05359a9168a122bcb8f7f6f9
DanteTemplar <69670642+DanteTemplar@users.noreply.github.com>
2020-11-30 18:00:36 +0700

    тыкаемся

---
+++ musercpavlov.py
@@ -0,0 +1,9 @@
+import deeppavlov
+config_path = r'F:\Codes\Projects\ML\NTI\AI\rubert_cased_L-12_H-768_A-12_v2\bert_config.json'
+#model = deeppavlov.build_model(config_path, download=True)
+
+# get predictions for 'input_text1', 'input_text2'
+#model(['input_text1', 'input_text2'])
+
+
+help(deeppavlov)
